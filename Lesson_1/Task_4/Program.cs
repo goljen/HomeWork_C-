5edea2dff243faf7cb6d028e48b0a7cbce66d7ce
GolJen <goljen@gmail.com>
2022-08-26 23:41:33 +0300

    Add Task 4 in Lesson 1

---
+++ Lesson_1/Task_4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Вы ввели число ");
+Console.WriteLine(number);
+
+int counter = 1;
+while (counter <= number)
+{
+    if (counter % 2 == 0)
+    {    
+        Console.Write(counter);
+        Console.Write(", ");  
+    } 
+    counter += 1;
+}
+Console.WriteLine();
