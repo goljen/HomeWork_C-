2932f00727dac7039796e2144d197517445b60c4
GolJen <goljen@gmail.com>
2022-08-26 23:17:47 +0300

    Add Task 3 in Lesson 1

---
+++ Lesson_1/Task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа, которая на вход принимает число и выдаёт, является ли число чётным. 
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Вы ввели число ");
+Console.WriteLine(number);
+if (number % 2 == 0)
+{
+    Console.Write("Число ");
+    Console.Write(number);
+    Console.WriteLine(" четное");
+}
+else
+{
+    Console.Write("Число ");
+    Console.Write(number);
+    Console.WriteLine(" нечетное"); 
+}
