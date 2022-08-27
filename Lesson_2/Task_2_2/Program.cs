953004f261e71767f759ca8d6a7f95b2e07f4098
GolJen <goljen@gmail.com>
2022-08-27 23:47:17 +0300

    Add Task 2_2

---
+++ Lesson_2/Task_2_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите целое положительное число: ");
+string number = Convert.ToString(Console.ReadLine());
+Console.Write("Вы ввели число ");
+Console.WriteLine(number);
+
+if (number.Length < 3)
+    Console.WriteLine("Третьей цифры в числе нет.");
+else
+{
+    char digit3 = number[2];
+    Console.Write("Третья цифра в числе равна ");
+    Console.WriteLine(digit3);
+}
+    
