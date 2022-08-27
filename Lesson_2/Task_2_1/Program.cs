6b95e11309d48949f701509a5c785efa95ac282b
GolJen <goljen@gmail.com>
2022-08-27 22:33:13 +0300

    Add Task 2_1

---
+++ Lesson_2/Task_2_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите целое трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Вы ввели число ");
+Console.WriteLine(number);
+
+int digit2_3 = number / 10;
+int digit2 = digit2_3 % 10;
+Console.Write("Вторая цифра числа ");
+Console.Write(number);
+Console.Write(" = ");
+Console.WriteLine(digit2);
+
