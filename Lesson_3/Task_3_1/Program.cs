b0a3d952fe9aad177e1de891a41fce697b8ceb2b
GolJen <goljen@gmail.com>
2022-08-29 15:30:23 +0300

    Add Task 3.1

---
+++ Lesson_3/Task_3_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+
+Console.Write("Введите целое положительное пятизначное число: ");
+string number = Convert.ToString(Console.ReadLine());
+Console.Write("Вы ввели число ");
+Console.WriteLine(number);
+
+if (number[0] == number[4] && number[1] == number[3])
+    Console.WriteLine("Это полиндром.");
+else
+    Console.WriteLine("Введённое число не являеться полиндромом.");
