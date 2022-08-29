5c9b35d1070b4b02de0b7973701fd248e9a75446
GolJen <goljen@gmail.com>
2022-08-29 22:49:10 +0300

    Add Task 3.3

---
+++ Lesson_3/Task_3_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+Console.Write("Таблица кубов чисел от 1 до  ");
+Console.WriteLine(number);
+while (count <= number)
+{
+   Console.Write(Math.Pow(count, 3));
+   Console.Write(", ");
+   count ++;
+}
+Console.WriteLine();
