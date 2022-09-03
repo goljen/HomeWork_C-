19b1351883f7f37f08f45c865ad480a472af2e96
GolJen <goljen@gmail.com>
2022-09-04 01:15:08 +0300

    Add in Task 4.3

--- Lesson_4/Task_4_3/Program.cs
+++ Lesson_4/Task_4_3/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int [] CreatArray(int size)
+{
+    int [] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+    return array;
+}   
+
+void WriteArray(int [] array)
+{
+    int count = array.Length;
+    Console.Write($"Массив из {count} элементов: ");
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        if (i == count - 1) 
+            Console.Write($"{array[i]}");
+        else 
+            Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine("]");
+}
+
+Console.Write("Введите размер массива: ");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number <= 0)
+    {
+        Console.Write("ОШИБКА!!! Размер массива должен быть больше 0. ");
+        Console.Write("Введите размер массива: ");
+        number = Convert.ToInt32(Console.ReadLine());
+    }
+int [] RandomAarray = new int [number];
+RandomAarray = CreatArray(number);
+WriteArray(RandomAarray);
+
