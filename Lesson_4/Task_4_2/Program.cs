b27f10ce3904e27a3365a3946cc58bd983a16fc4
GolJen <goljen@gmail.com>
2022-09-03 23:52:35 +0300

    Add Task 4.2

---
+++ Lesson_4/Task_4_2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Программа, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int ReadNaturalNumber()
+{
+    Console.Write("Введите натуральное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Вы ввели число {number}");
+    Console.WriteLine();
+
+    return number;
+}
+
+int SumDigit(int num)
+{
+   int sum = 0;
+   while (num > 0) 
+   {
+        sum += num % 10;
+        num /= 10;
+   }
+   return sum;
+}
+
+
+
+
+int a = ReadNaturalNumber();
+int SummaDigit = SumDigit(a);
+Console.WriteLine($"Сумма цифр числа {a} равна {SummaDigit}");
+
+
+
