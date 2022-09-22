7abb9e0a031ac3d2b352c4a6ca7cd0e7d56b1f0c
GolJen <goljen@gmail.com>
2022-09-22 18:24:38 +0300

    Add Task 7.1

---
+++ Lesson_7/Task_7_1/Program.cs
@@ -0,0 +1,44 @@
+﻿//  Программа создает двумерный массив размером m×n, заполненный случайными
+//  вещественными числами.
+
+double [,] CreateArray(int number1, int number2)
+{
+    double [,] array = new double[number1, number2];
+    for(int i = 0; i < number1; i++)
+    {
+        for(int j = 0; j < number2; j++)
+        array[i, j] = Convert.ToDouble(new Random().Next(-100, 100) / 10.0);
+    }
+    return array;
+}
+
+void WriteArray(double [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j ++)
+        {
+            Console.Write(array[i, j]);
+            Console.Write(" ");  
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк массива:");
+int numberRows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива:");
+int numberColumns = Convert.ToInt32(Console.ReadLine());
+while(numberRows <= 0 || numberColumns <= 0)
+{
+    Console.WriteLine("ОШИБКА!!!");
+    Console.WriteLine("Количество строк и столбцов массива должно быть больше 0 ");
+    Console.WriteLine("Попробуйте ещё раз.");
+    Console.Write("Введите количество строк массива:");
+    numberRows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количество столбцов массива:");
+    numberColumns = Convert.ToInt32(Console.ReadLine());
+}
+
+double [,] newArray = CreateArray(numberRows, numberColumns);
+WriteArray(newArray);
